632f7ee53158b14a70a29a97e71f6d121725b158
Daniel Russom <daniel.russom00@gmail.com>
2018-02-09 20:18:45 +0000

    Translator Init
    -Added template for the translator class

---
+++ src/application/util/MorseCodeTranslator.java
@@ -0,0 +1,29 @@
+package application.util;
+
+public class MorseCodeTranslator {
+	//TODO Either ArrayList or Hashmap for translation
+
+	/**
+	 * Translates the passed in message to Morse Code
+	 * 
+	 * @param english
+	 *            - original message to be translated
+	 * @return - the passed in message translated to Morse Code
+	 */
+	public static String translateToMorseCode(String english) {
+
+		return null;
+	}
+
+	/**
+	 * Translates the passed in message to English
+	 * 
+	 * @param morseCode
+	 *            - original message to be translated
+	 * @return - the passed in message translated to English
+	 */
+	public static String translateToEnglish(String morseCode) {
+
+		return null;
+	}
+}
