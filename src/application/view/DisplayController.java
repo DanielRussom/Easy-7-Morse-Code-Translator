13bd34fe03a57a31c2c2ddaaee2428374524e192
Daniel Russom <daniel.russom00@gmail.com>
2018-02-15 23:42:14 +0000

    Audio Checkbox
    -Added checkbox to toggle audio output

--- src/application/view/DisplayController.java
+++ src/application/view/DisplayController.java
@@ -2,24 +2,33 @@ package application.view;
 
 import application.util.MorseCodeTranslator;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextArea;
 
 public class DisplayController {
 
 	@FXML
 	private TextArea taMorseCode;
-	
-	@FXML private TextArea taEnglish;
+
+	@FXML
+	private TextArea taEnglish;
 	
 	@FXML
-	public void translateToMorseCode(){
+	private CheckBox cbAudio;
+
+	@FXML
+	public void translateToMorseCode() {
 		String textEnglish = MorseCodeTranslator.translateToMorseCode(taEnglish.getText());
 		taMorseCode.setText(textEnglish);
 	}
-	
+
 	@FXML
-	public void translateToEnglish(){
+	public void translateToEnglish() {
 		String textMorse = MorseCodeTranslator.translateToEnglish(taMorseCode.getText());
 		taEnglish.setText(textMorse);
+	}
+	
+	public boolean isAudioEnabled() {
+		return cbAudio.isSelected();
 	}
 }
